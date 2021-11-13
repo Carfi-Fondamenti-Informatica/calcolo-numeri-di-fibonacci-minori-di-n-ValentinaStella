b87854bbe2802d7326ac8a56180066299ea82918
ValentinaStella <91681272+ValentinaStella@users.noreply.github.com>
2021-11-13 16:56:42 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 
 int main() {
+    int n=0, x=0, y=1, z=x+y;
+    cin>>n;
+    cout<<1<<endl;
+    for (int i = 0; i< (n-1); ++i) {
+        z=x+y;
+        x=y;
+        y=z;
+        cout<<z<<endl;
+    }
    return 0;
 }
